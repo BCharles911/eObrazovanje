2077579ff3e743d831a920491672b92c74e59ce3
BCharles911 <djapic.andrej@gmail.com>
2020-12-03 18:40:18 +0100

    uradjeno dodavanje nastavnika na predmet,
    proveriti 87. liniju u lecturerController klasi za vise detalja

--- ssluzba/src/main/java/com/eobrazovanje/ssluzba/controller/LecturerController.java
+++ ssluzba/src/main/java/com/eobrazovanje/ssluzba/controller/LecturerController.java
@@ -6,15 +6,19 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.concurrent.SuccessCallback;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.eobrazovanje.ssluzba.dto.LecturerDTO;
 import com.eobrazovanje.ssluzba.dto.converter.LecturerToDTO;
+import com.eobrazovanje.ssluzba.dto.toEntityConverters.DTOToLecturer;
 import com.eobrazovanje.ssluzba.entities.Lecturer;
 import com.eobrazovanje.ssluzba.entities.Subject;
 import com.eobrazovanje.ssluzba.repository.LecturerRepository;
@@ -33,6 +37,9 @@ public class LecturerController {
 	
 	@Autowired
 	LecturerToDTO lecturerToDTO;
+	
+	@Autowired
+	DTOToLecturer dtoToLecturer;
 	
 	
 	
@@ -75,6 +82,21 @@ public class LecturerController {
 		}
 		
 		return new ResponseEntity<List<LecturerDTO>>(lecturerToDTO.convert(toReturnLecturers), HttpStatus.OK);
+	}
+	
+	@PostMapping(value="add-to-subject", consumes="application/json")
+	public ResponseEntity<?> addToSubject(@RequestBody List<LecturerDTO> lecturersToAdd, @RequestParam("subjectId") Long id){
+		Subject subject = subjectRepository.getOne(id);
+		
+		List<Lecturer> lecturers = new ArrayList<Lecturer>();
+		for(LecturerDTO l : lecturersToAdd) {
+			lecturers.add(dtoToLecturer.convert(l));
+		}
+		for(Lecturer l : lecturers) {
+			subject.getLecturer().add(l);
+		}
+		subjectRepository.save(subject);
+		return new ResponseEntity<Subject>(subject, HttpStatus.OK);
 	}
 
 
